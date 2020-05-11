a14ac2722093f4a34192a809b38aa54a653c465d
Михаил Михайлов <mixajlov.mikhail@mmwebstudio.pp.ua>
2020-05-11 19:04:40 +0300

    -исправлен парсинг ссылок на следуюющую страницу sikoauktioner

--- getNextPageLink.js
+++ getNextPageLink.js
@@ -2,7 +2,15 @@ var result = false;
 var phost = location.protocol+'//'+location.hostname;
 if(qt.jQuery('{next_page_selector}').length > 0)
 {
-    result = qt.jQuery('{next_page_selector}').attr("href");
+    if(qt.jQuery('{next_page_selector}')[0].tagName == "a")
+    {
+        result = qt.jQuery('{next_page_selector}').attr("href");
+    }
+    else
+    {
+        result = qt.jQuery('{next_page_selector}').closest("a").next("a").attr("href");
+    }
+
     if(result.indexOf(location.hostname) == -1) {
         result = phost + result;
     }
